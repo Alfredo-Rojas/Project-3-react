3e291f7d831a509f86bce17202638a3245584e33
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-31 18:20:19 -0400

    Initial Login form created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import SignUp from './components/signup/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <SignUp />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listOfChatRoom: [],
+      currentlyLoggedIn: null,
+      ready: false,
+      signupShowing: false,
+      loginShowing: false,
+    }
+    return (
+      <div className="App">
+        <SignUp />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import AuthService from "../../services/AuthService";
 
-export class Login extends Component {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { usernameInput: "", passwordInput: "" };
+    this.service = new AuthService();
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  tryToLogin = e => {
+    e.preventDefault();
+    const uName = this.state.usernameInput;
+    const pword = this.state.passwordInput;
+
+    this.service.login(uName, pWord).then(() => {
+      this.props.toggleForm("login");
+      this.props.getUser();
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      <form onSubmit={this.tryToLogin}>
+        <h3>login</h3>
+
+        <legend>Username</legend>
+        <input
+          value={this.state.usernameInput}
+          name="usernameInpit"
+          onChange={this.handleChange}
+        />
+
+        <legend>Password</legend>
+        <input
+          value={this.state.passwordInput}
+          name="passwordInput"
+          onChange={this.handleChange}
+        />
+      </form>
+    );
   }
 }
 
