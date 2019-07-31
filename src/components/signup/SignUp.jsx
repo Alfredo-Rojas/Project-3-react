2af96288801495dad56d53c38686b2d53817e3f1
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-31 17:39:07 -0400

    Initial Signup form created

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
+import AuthService from "../../services/AuthService";
+import { FormControl } from "@material-ui/core";
 
-export class SignUp extends Component {
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", passwordInput: "" };
+    this.service = new AuthService();
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  tryToSignUp = e => {
+    e.preventDefault();
+    const uName = this.state.usernameInput;
+    const pWord = this.state.passwordInput;
+
+    this.service.signup(uName, pWord).then(() => {
+      this.props.toggleForm("signup");
+      this.props.getUser();
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      // this form is temporal
+      // i'll change it for a more stylized one
+      <form onSubmit={this.trySignUp}>
+        <h3>Signup For An Account</h3>
+
+        <legend>Username</legend>
+        <input
+          value={this.state.passwordInput}
+          name="usernameInput"
+          onChange={this.handleChange}
+        />
+
+        <legend>Password</legend>
+        <input
+          value={this.state.passwordInput}
+          name="passwordInput"
+          onChange={this.handleChange}
+        />
+
+        <button>Submit</button>
+      </form>
+    );
   }
 }
 
