e3991f18ed3123b9640d05770c489f914af1c981
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-31 16:10:26 -0400

    Service auth credentials created

---
+++ src/components/addchatroom/AddChatRoom.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class AddChatRoom extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default AddChatRoom;
---
+++ src/services/AuthService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+class AuthService {
+  coonstructor() {
+    let service = axios.create({
+      baseURL: 'http://localhost:5000/api/auth',
+      withCredentials: true
+    });
+    this.service = service;
+  }
+
+  signup = (username, password) => {
+    return this.service.post('/signup', {username:username, password:password})
+    .then(response => response.data)
+  }
+
+  login = (username, password) => {
+    return this.servie.post('/login', {username, password})
+    .then(response => response.data)
+  }
+
+  currentUser = () => {
+    return this.service.get('/getcurrentuser')
+    .then(response => response.data)
+  }
+
+  logout = () => {
+    return this.service.post('/logout', {})
+    .then(response => response.data)
+  }
+}
+
+
+
+export default AuthService;
