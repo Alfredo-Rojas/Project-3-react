df4676d43975238f8739f97fd9fad03566f20155
Alfredo-Rojas <alfredot84@gmail.com>
2019-08-01 16:04:15 -0400

    Meet Up component created

--- src/components/addmeetup/AddMeetUp.jsx
+++ src/components/addmeetup/AddMeetUp.jsx
@@ -1,8 +1,124 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-export class AddMeetUp extends Component {
+class AddMeetUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTitle: "",
+      newDescription: "",
+      newType: "",
+      newStreetAddress: "",
+      newCity: "",
+      newState: "",
+      newCountry: "",
+      newTime: ""
+    };
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    axios
+      .post(
+        "http://localhost:5000/api/meetup",
+        {
+          theTitle: this.state.newTitle,
+          theDescription: this.state.newDescription,
+          theType: this.state.newType,
+          theStreetAddress: this.state.newStreetAddress,
+          theCity: this.state.newCity,
+          theState: this.state.newState,
+          theCountry: this.state.newCountry,
+          theTime: this.state.newTime
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        this.props.getData();
+        // This function updates the list in MeetUpIndex.js
+        this.setState({
+          newTitle: "",
+          newDescription: "",
+          newType: "",
+          newStreetAddress: "",
+          newCity: "",
+          newState: "",
+          newCountry: "",
+          newTime: ""
+        });
+      })
+      .catch(error => console.log(error));
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
-    return <div />;
+    return (
+      <div className="add-meetup">
+        <form inSubmit={this.handleFormSubmit}>
+          <label>Title:</label>
+          <input
+            type="text"
+            name="newTitle"
+            value={this.state.newTitle}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>Description:</label>
+          <textarea
+            name="newDescription"
+            value={this.state.newDescription}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>Type:</label>
+          <input
+            type="text"
+            name="newType"
+            value={this.state.newType}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>Street Address:</label>
+          <input
+            type="text"
+            name="newStreetAddress:"
+            value={this.state.newStreetAddress}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>City:</label>
+          <input
+            type="text"
+            name="newCity"
+            value={this.state.newCity}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>State:</label>
+          <input
+            type="text"
+            name="newState"
+            value={this.state.newState}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>Country:</label>
+          <input
+            type="text"
+            name="newCountry"
+            value={this.state.newCountry}
+            onChange={e => this.handleChange(e)}
+          />
+          <label>Time:</label>
+          <input
+            type="text"
+            name="newTime"
+            value={this.state.newTime}
+            onChange={e => this.handleChange(e)}
+          />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
